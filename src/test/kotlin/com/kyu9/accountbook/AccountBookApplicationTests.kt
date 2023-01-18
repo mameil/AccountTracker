5bd56e7c2cec45de71ec88e6e7cdffd892a811c2
kyu9 <kyudo97@naver.com>
2023-01-18 20:48:26 +0900

    Project basic setting

---
+++ src/test/kotlin/com/kyu9/accountbook/AccountBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kyu9.accountbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
