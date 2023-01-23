30e848326a7ff1364de5cc9b8736909f3a7a9492
kyu9 <kyudo97@naver.com>
2023-01-23 13:41:51 +0900

    utils - ADD merge all into master shell script

---
+++ script/merge_all_branch_to_MASTER.sh
@@ -0,0 +1,17 @@
+git branch --list >> list.txt
+array=(`cat list.txt | tr -d '' | tr -d '*'`)
+
+printf "go do MASTER branch \r"
+git checkout master;
+
+for i in "${array[@]}" ; do
+    if [ i != "master" ]; then
+      printf "======= Merge from $i into MASTER ======= \r"
+      git merge $i;
+    fi
+done
+
+git pull;
+git push;
+
+rm list.txt
