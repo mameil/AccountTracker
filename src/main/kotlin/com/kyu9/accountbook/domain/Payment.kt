659d308b43bf1b9bd0f86441530a00cbcfd40757
kyu9 <kyudo97@naver.com>
2023-01-23 21:15:29 +0900

    domain - rename field, ddl update

--- src/main/kotlin/com/kyu9/accountbook/domain/Payment.kt
+++ src/main/kotlin/com/kyu9/accountbook/domain/Payment.kt
@@ -1,14 +1,16 @@
 package com.kyu9.accountbook.domain
 
 import com.kyu9.accountbook.common.BaseEntity
-import com.kyu9.accountbook.common.DefaultValue
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import java.time.LocalDateTime
 
 @Entity
-data class Usage(
+data class Payment(
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
     val amount: Long,
     val registered: LocalDateTime,
--- src/main/kotlin/com/kyu9/accountbook/domain/Tag.kt
+++ src/main/kotlin/com/kyu9/accountbook/domain/Tag.kt
@@ -2,12 +2,15 @@ package com.kyu9.accountbook.domain
 
 import com.kyu9.accountbook.common.BaseEntity
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import java.time.LocalDateTime
 
 @Entity
 data class Tag(
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
     val name: String,
     val color: String,
