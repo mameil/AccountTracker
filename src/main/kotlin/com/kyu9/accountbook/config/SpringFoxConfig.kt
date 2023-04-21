fbd63a0e341db112a1d98c69c0167d67fae35c8b
kyu9 <kyudo97@naver.com>
2023-04-21 17:56:02 +0900

    app - springfox view url added

--- src/main/kotlin/com/kyu9/accountbook/config/SpringFoxConfig.kt
+++ src/main/kotlin/com/kyu9/accountbook/config/SpringFoxConfig.kt
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.VendorExtension
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger.web.TagsSorter
@@ -28,7 +29,7 @@ class SpringFoxConfig {
     private fun apiInfo(): ApiInfo? {
         return ApiInfoBuilder()
                 .title("AccountTracker API")
-                .description("Kyudo's AccountBook by kotlin springboot")
+                .description("Kyudo's AccountBook by kotlin springboot <br>main views<br> localhost:10001/view/login")
                 .version("1.0")
                 .build()
     }
