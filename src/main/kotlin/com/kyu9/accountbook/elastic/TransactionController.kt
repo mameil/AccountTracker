168646e9e1355a51f8923431dd9e13553d23be9f
NineKy <kyudo97@naver.com>
2023-07-09 10:27:06 +0900

    app - ES index post controller list version added

--- src/main/kotlin/com/kyu9/accountbook/elastic/TransactionController.kt
+++ src/main/kotlin/com/kyu9/accountbook/elastic/TransactionController.kt
@@ -5,6 +5,7 @@ import com.kyu9.accountbook.elastic.dto.PostTranReqDto
 import com.kyu9.accountbook.swagger.model.PostTranRequestDto
 import com.kyu9.accountbook.swagger.model.PostTransResponseDto
 import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -30,6 +31,17 @@ class TransactionController(
     fun postTransaction( @Valid @RequestBody dto: PostTranReqDto
     ): ResponseEntity<String> {
         return ResponseEntity.ok(transactionService.postTransaction(dto.userId, dto.userName, dto.amt))
+    }
+
+    @PostMapping(
+            value = ["/transaction/list"],
+            produces = ["application/json"],
+            consumes = ["application/json"]
+    )
+    fun postListTransaction( @Valid @RequestBody dto: List<PostTranReqDto>
+    ): ResponseEntity<Void> {
+        dto.forEach { transactionService.postTransaction(it.userId, it.userName, it.amt) }
+        return ResponseEntity(HttpStatus.OK)
     }
 
     @GetMapping("/transaction/{id}")
