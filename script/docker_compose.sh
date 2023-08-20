3788b516950eb19d640ecc622d01b18400ac8598
NineKy <kyudo97@naver.com>
2023-08-20 15:22:54 +0900

    app - docker shellscript fix, application.yml fix

--- script/docker_compose.sh
+++ script/docker_compose.sh
@@ -4,8 +4,8 @@
 # create image
 docker build --platform linux/amd64 --tag=myapp:latest .
 
-# run container
-#~~
+# run container - 외부에서 10002로 들어가면 docker 내에 10001에 접근
+docker run -d --name KD-acb --network kd-network -p 10002:10001 myapp
 
 # delete container
 docker rm KD-acb
