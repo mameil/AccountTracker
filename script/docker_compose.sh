ddec36a4d16ab0bc68392b4ed187e6d910f245a0
NineKy <kyudo97@naver.com>
2023-09-02 11:53:46 +0900

    common - added echo while making new jar

--- script/docker_compose.sh
+++ script/docker_compose.sh
@@ -1,26 +1,27 @@
-# stop current container
+echo >> stop current container
 docker stop KD-acb
 
-# delete container
+echo >> delete current container
 docker rm KD-acb
 
-# image delete
+echo >> delete old image
 docker rmi myapp
 
-# make new jar
+echo >> create new jar
 ./gradlew clean build
 
-# create new image from recent jar
+echo >> create new image from recent jar
 docker build --platform linux/amd64 --tag=myapp:latest .
 
-# run container - 외부에서 10002로 들어가면 docker 내에 10001에 접근
+echo >> run new container by new image
+## - 외부에서 10002로 들어가면 docker 내에 10001에 접근
 docker run -d --name KD-acb --network kd-network -p 10002:10001 myapp
 
-# check created container
+echo >> check created container
 docker ps
 
-# check health UP
+echo >> check new health of container UP
 curl -X GET localhost:10002/acb/actuator/health -v
 
-# check latest git commit
+echo >> check latest git commit
 curl -X GET localhost:10002/acb/actuator/git -v
