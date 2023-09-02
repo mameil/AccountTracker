f5ec53eec606c693481e099447b8cf2e31f6b983
NineKy <kyudo97@naver.com>
2023-09-02 12:44:40 +0900

    common - customize actuator > added git information for versioning jar

--- src/main/kotlin/com/kyu9/accountbook/config/CustomActuatorConfig.kt
+++ src/main/kotlin/com/kyu9/accountbook/config/CustomActuatorConfig.kt
@@ -3,14 +3,29 @@ package com.kyu9.accountbook.config
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.stereotype.Component
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 
 @Component
 class CustomActuatorConfig : HealthIndicator {
     override fun health(): Health {
         val map: MutableMap<String, String> = HashMap()
-        map["foo"] = "bar"
-        map["my"] = "value"
-        return Health.Builder().up().withDetail("simple", map).build()
+
+        var commitMsgFromGit: Process = Runtime.getRuntime().exec("git log --format=%B -n 1")
+        var gitMsg: String = ""
+        BufferedReader(InputStreamReader(commitMsgFromGit.inputStream)).use {
+            reader -> gitMsg = reader.readLine()
+        }
+
+        var commitHashShortFromGit: Process = Runtime.getRuntime().exec("git log -n 1 --abbrev-commit --format=%h")
+        var gitHashShort: String = ""
+        BufferedReader(InputStreamReader(commitHashShortFromGit.inputStream)).use {
+            reader -> gitHashShort = reader.readLine()
+        }
+
+        map["git_message"] = gitMsg
+        map["git_hash_short"] = gitHashShort
+        return Health.Builder().up().withDetail("git", map).build()
     }
 }
