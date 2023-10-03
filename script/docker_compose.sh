f213abe87c57f1ff3d0851aa03f904b7f8a9f057
NineKy <kyudo97@naver.com>
2023-10-03 20:58:57 +0900

    app - docker shellscript fixed

--- script/docker_compose.sh
+++ script/docker_compose.sh
@@ -1,21 +1,29 @@
 echo >> stop current container
 docker stop KD-acb
+sleep 1
 
 echo >> delete current container
 docker rm KD-acb
+sleep 1
 
 echo >> delete old image
 docker rmi myapp
+sleep 1
+
+cd ../
 
 echo >> create new jar
 ./gradlew clean build
+sleep 1
 
 echo >> create new image from recent jar
 docker build --platform linux/amd64 --tag=myapp:latest .
+sleep 1
 
 echo >> run new container by new image
 ## - 외부에서 10002로 들어가면 docker 내에 10001에 접근
 docker run -d --name KD-acb --network kd-network -p 10002:10001 myapp
+sleep 10
 
 ## 디폴트로 만들어진 리눅스는 /etc/os-release 을 통해서 확인해보면 Alpine 리눅스 버전이고 해당 버전에서는 apk install ~~ 이러한 문법을 통해서 원하는걸 다운받는 것이 가능ㅎ
 
